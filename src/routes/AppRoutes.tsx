a7a3a270a4b949f4734840ed2591e3399b6f9f86
Ana Volkmann <melodyycat@gmail.com>
2026-06-10 17:09:58 -0300

    removidas páginas sobressalentes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Servico from "@/pages/Servicos";
-import Cadastro from "@/pages/Cadastro";
-import ConfirmaçãoHorário from "@/pages/ConfirmaçãoHorário";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Servico />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/confirmacao" element={<ConfirmaçãoHorário />} />
       </Routes>
     </BrowserRouter>
   );
